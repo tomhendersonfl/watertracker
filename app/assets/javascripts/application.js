07becb51ae4027cd218b0d4d45167dbcb83aedbf
AA <tomhendersonfl@gmail.com>
2016-10-08 19:50:50 -0600

    enhancements

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,40 +1,16 @@
-// This is a manifest file that'll be compiled into application.js, which will include all the files
-// listed below.
-//
-// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
-// or any plugin's vendor/assets/javascripts directory can be referenced here using a relative path.
-//
-// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
-// compiled file. JavaScript code in this file should be added after the last require_* statement.
-//
-// Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
-// about supported directives.
-//
-//= require jquery
-//= require jquery.turbolinks
+//= require bower_components/jquery/dist/jquery.min
 //= require jquery_ujs
-//= require turbolinks
-//
-//= require vendors/bower_components/bootstrap/dist/js/bootstrap.min
-//= require vendors/bower_components/flot/jquery.flot
-//= require vendors/bower_components/flot/jquery.flot.resize
-//= require vendors/bower_components/flot.curvedlines/curvedLines
-//= require vendors/bower_components/jquery.easy-pie-chart/dist/jquery.easypiechart.min
-//= require vendors/sparklines/jquery.sparkline.min
-//= require vendors/bower_components/moment/min/moment.min
-//= require vendors/bower_components/fullcalendar/dist/fullcalendar.min
-//= require vendors/bower_components/simpleWeather/jquery.simpleWeather.min
-//= require vendors/bower_components/Waves/dist/waves.min
-//= require vendors/bootstrap-growl/bootstrap-growl.min
-//= require vendors/bower_components/sweetalert/dist/sweetalert.min
-//= require vendors/bower_components/malihu-custom-scrollbar-plugin/jquery.mCustomScrollbar.concat.min
-//= require vendors/bower_components/jquery-placeholder/jquery.placeholder.min
-//= require bootgrid/jquery.bootgrid.updated.min
+//= require jquery
+//= require bower_components/bootstrap/dist/js/bootstrap.min
+//= require bower_components/Waves/dist/waves.min
+//= require bower_components/malihu-custom-scrollbar-plugin/jquery.mCustomScrollbar.concat.min
 
-//
+//= require theme
+
+//= require bower_components/bootstrap-select/dist/js/bootstrap-select
+//= require bower_components/chosen/chosen.jquery
+//= require bootgrid/jquery.bootgrid.updated.min
+//= require bower_components/autosize/dist/autosize.min
+
+
 //= require functions/bootgrid
-//= require bootstrap-sprockets
-//= require cable
-//= require js/app.min
-//
-//= require_tree .
