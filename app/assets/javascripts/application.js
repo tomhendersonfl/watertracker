d5e831e241351823d54f1514b932f3258ab3bdc4
AA <tomhendersonfl@gmail.com>
2016-10-10 21:05:52 -0600

    enhancements

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,4 +1,34 @@
 //= require bower_components/jquery/dist/jquery.min
-//= require jquery
 //= require jquery_ujs
+//= require jquery
 //= require bower_components/bootstrap/dist/js/bootstrap.min
+//= require bower_components/flot/jquery.flot
+//= require bower_components/flot/jquery.flot.resize
+//= require bower_components/flot.curvedlines/curvedLines
+//= require sparklines/jquery.sparkline.min
+//= require bower_components/jquery.easy-pie-chart/dist/jquery.easypiechart.min
+//= require bower_components/moment/min/moment.min
+//= require bower_components/fullcalendar/dist/fullcalendar.min
+//= require bower_components/Waves/dist/waves.min
+//= require bootstrap-growl/bootstrap-growl.min
+//= require bower_components/sweetalert/dist/sweetalert.min
+//= require bower_components/malihu-custom-scrollbar-plugin/jquery.mCustomScrollbar.concat.min
+//= require bower_components/nouislider/distribute/nouislider.min
+//= require readmore-js/readmore.min
+
+//= require theme
+
+
+//= require bower_components/bootstrap-select/dist/js/bootstrap-select
+//= require bower_components/chosen/chosen.jquery
+//= require fileinput/fileinput.min
+//= require summernote/dist/summernote-updated.min
+//= require input-mask/input-mask.min
+//= require farbtastic/farbtastic.min
+//= require bower_components/jquery.hotkeys/jquery.hotkeys
+//= require bootgrid/jquery.bootgrid.updated.min
+//= require bower_components/autosize/dist/autosize.min
+
+
+//= require functions/bootgrid
+//= require cable
